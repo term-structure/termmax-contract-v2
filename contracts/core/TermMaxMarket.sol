4986e124d251b0318fe6da8a722c0d971cdf00e7
Evan Wang <evan.wang@tkspring.com>
2024-11-18 01:36:07 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: NIC Lin <twedusuck@gmail.com>

--- contracts/core/TermMaxMarket.sol
+++ contracts/core/TermMaxMarket.sol
@@ -59,6 +59,7 @@ contract TermMaxMarket is ITermMaxMarket, ReentrancyGuard, Ownable, Pausable {
         IGearingToken gt_,
         MarketConfig memory config_
     ) external override {
+        // __initializeOwner will revert if already initialized
         __initilizeOwner(admin);
         if (address(collateral_) == address(underlying_)) {
             revert CollateralCanNotEqualUnserlyinng();
