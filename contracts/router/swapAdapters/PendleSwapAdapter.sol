5aa348da209a21e1cc35053d7a1fe625f4354ee5
evan <evan@gmail.com>
2024-11-18 15:55:39 +0800

    pendle helper

---
+++ contracts/router/swapAdapters/PendleSwapAdapter.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PendleHelper} from "../lib/PendleHelper.sol";
+import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
+import {IPMarket} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
+import {ISwapAdapter} from "../ISwapAdapter.sol";
+
+contract PendleSwapAdapter is ISwapAdapter, PendleHelper {}
