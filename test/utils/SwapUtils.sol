14ac528862153f55fff982b394faa1c2ceab7bc4
evan <evan@gmail.com>
2025-01-06 17:01:13 +0800

    test utils

--- test/utils/SwapUtils.sol
+++ test/utils/SwapUtils.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.27;
 
 import {console} from "forge-std/Script.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import {DeployUtils, Constants} from "./DeployUtils.sol";
+import {Constants} from "contracts/lib/Constants.sol";
+import {DeployUtils} from "./DeployUtils.sol";
 
 library SwapUtils {
     using SafeCast for uint256;
     using SafeCast for int256;
 
     function getPrice(DeployUtils.Res memory res) internal view returns (uint256 pFt, uint256 pXt) {
-        (uint lendApr_, uint borrowApr_) = res.market.apr();
+        (uint lendApr_, uint borrowApr_) = res.order.apr();
 
-        uint dtm = daysToMaturity(res.tokenPair.config().maturity);
+        uint dtm = daysToMaturity(res.market.config().maturity);
         pFt = Constants.DECIMAL_BASE_SQ / (Constants.DECIMAL_BASE + (lendApr_ * dtm) / Constants.DAYS_IN_YEAR);
         pXt = Constants.DECIMAL_BASE - pFt / Constants.DECIMAL_BASE;
     }
