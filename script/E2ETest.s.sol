ffaadb8a150f5357aed15e69ce2d21830f52cecc
Aaron C.-W. Liang <aaron.liang@tkspring.com>
2025-02-12 22:21:22 +0800

    update e2e script

--- script/E2ETest.s.sol
+++ script/E2ETest.s.sol
@@ -28,8 +28,10 @@ import {MarketConfig} from "../contracts/storage/TermMaxStorage.sol";
 
 contract E2ETest is Script {
     // deployer config
-    uint256 userPrivateKey = vm.envUint("ARB_SEPOLIA_DEPLOYER_PRIVATE_KEY");
-    address userAddr = vm.addr(userPrivateKey);
+    uint256 deployerPrivateKey = vm.envUint("ARB_SEPOLIA_DEPLOYER_PRIVATE_KEY");
+    address deployerAddr = vm.addr(deployerPrivateKey);
+    address userAddr;
+    uint256 userPrivateKey;
 
     // address config
     address faucetAddr = address(0x5adA709210846DA33E3866490EAFB90B7ea96f7f);
@@ -53,46 +55,76 @@ contract E2ETest is Script {
     address underlyingPriceFeedAddr;
 
     function run() public {
+        (userAddr, userPrivateKey) = makeAddrAndKey("New User1");
+        vm.startBroadcast(deployerPrivateKey);
+        payable(userAddr).transfer(0.1 ether);
+        vm.stopBroadcast();
         (ft, xt, gt, collateralAddr, underlyingERC20) = market.tokens();
         collateral = FaucetERC20(collateralAddr);
         underlying = FaucetERC20(address(underlyingERC20));
         underlyingPriceFeedAddr = faucet.getTokenConfig(faucet.getTokenId(address(underlying))).priceFeedAddr;
         collateralPriceFeedAddr = faucet.getTokenConfig(faucet.getTokenId(address(collateral))).priceFeedAddr;
         printMarketConfig();
-        mintDebtToken(100000);
+        mintDebtToken(deployerAddr, 100000);
         depositIntoOrder(100000);
-        mintDebtToken(1000);
-        lendToOrder(1000);
-        mintCollateralToken(12000);
-        uint256 gtId = borrowFromOrder(12000, 8000, 8500);
-        mintCollateralToken(100);
-        addCollateral(gtId, 100);
-        removeCollateral(gtId, 50);
-        mintDebtToken(500);
-        repay(gtId, 500, true);
-        transferGT(gtId, vm.randomAddress());
+
+        console.log("User addr:", userAddr);
+        console.log("User private key:", userPrivateKey);
         printUserPosition();
-        mintDebtToken(20004);
+
+        console.log("> Mint 21504 debt token");
+        mintDebtToken(userAddr, 21504);
+        printUserPosition();
+
+        console.log("> Mint 12100 collateral token");
+        mintCollateralToken(userAddr, 12100);
+        printUserPosition();
+        console.log("> Buy FT with 1000 debt token");
+        lendToOrder(1000);
+        printUserPosition();
+
+        console.log("> Borrow  8000 debt token with 12000 coll collateral token");
+        uint256 gtId = borrowFromOrder(12000, 8000, 8500);
+        printUserPosition();
+
+        console.log("> Add 100 collateral");
+        addCollateral(gtId, 100);
+        printUserPosition();
+
+        console.log("> Remove 50 collateral");
+        removeCollateral(gtId, 50);
+        printUserPosition();
+
+        console.log("> Repay 500 debt token");
+        repay(gtId, 500, true);
+        printUserPosition();
+
+        address to = vm.randomAddress();
+        console.log("> Transfer GT to", to);
+        transferGT(gtId, to);
+        printUserPosition();
+
+        console.log("> Buy XT with 4 debt token and do leverage with 20000 debt token");
         leverageFromOrder(4, 0, 20000, 0.8e8);
         printUserPosition();
     }
 
-    function mintDebtToken(uint256 amount) public {
+    function mintDebtToken(address to, uint256 amount) public {
         uint256 mintAmt = amount * 10 ** underlying.decimals();
-        vm.startBroadcast(userPrivateKey);
-        faucet.devMint(userAddr, address(underlying), mintAmt);
+        vm.startBroadcast(deployerPrivateKey);
+        faucet.devMint(to, address(underlying), mintAmt);
         vm.stopBroadcast();
     }
 
-    function mintCollateralToken(uint256 amount) public {
+    function mintCollateralToken(address to, uint256 amount) public {
         uint256 mintAmt = amount * 10 ** collateral.decimals();
-        vm.startBroadcast(userPrivateKey);
-        faucet.devMint(userAddr, address(collateral), mintAmt);
+        vm.startBroadcast(deployerPrivateKey);
+        faucet.devMint(to, address(collateral), mintAmt);
         vm.stopBroadcast();
     }
 
     function depositIntoOrder(uint256 depositAmt) public {
-        vm.startBroadcast(userPrivateKey);
+        vm.startBroadcast(deployerPrivateKey);
         depositAmt = depositAmt * 10 ** underlying.decimals();
         (uint256 oriFtReserve, uint256 oriXtReserve) = order.tokenReserves();
 
@@ -143,7 +175,11 @@ contract E2ETest is Script {
         // console.log("new ftBalance:", newFtBalance);
     }
 
-    function borrowFromOrder(uint256 collateralAmt, uint256 borrowAmt, uint256 maxDebtAmt) public returns (uint256 gtId){
+    function borrowFromOrder(
+        uint256 collateralAmt,
+        uint256 borrowAmt,
+        uint256 maxDebtAmt
+    ) public returns (uint256 gtId) {
         collateralAmt = collateralAmt * 10 ** collateral.decimals();
         borrowAmt = borrowAmt * 10 ** underlying.decimals();
         maxDebtAmt = maxDebtAmt * 10 ** underlying.decimals();
@@ -160,7 +196,14 @@ contract E2ETest is Script {
         orders[0] = order;
         uint128[] memory ftAmtsToSell = new uint128[](1);
         ftAmtsToSell[0] = uint128(borrowAmt);
-        gtId = router.borrowTokenFromCollateral(userAddr, market, collateralAmt, orders, ftAmtsToSell, uint128(maxDebtAmt));
+        gtId = router.borrowTokenFromCollateral(
+            userAddr,
+            market,
+            collateralAmt,
+            orders,
+            ftAmtsToSell,
+            uint128(maxDebtAmt)
+        );
         vm.stopBroadcast();
 
         (uint256 newFtReserve, uint256 newXtReserve) = order.tokenReserves();
@@ -224,7 +267,7 @@ contract E2ETest is Script {
         // console.log("new borrowApr:", newBorrowApr);
         // console.log("new underlyingBalance:", newUnderlyingBalance);
     }
-    
+
     function addCollateral(uint256 gtId, uint256 addCollateralAmt) public {
         addCollateralAmt = addCollateralAmt * 10 ** collateral.decimals();
         vm.startBroadcast(userPrivateKey);
@@ -252,8 +295,9 @@ contract E2ETest is Script {
         vm.startBroadcast(userPrivateKey);
         gt.transferFrom(userAddr, toAddr, gtId);
         vm.stopBroadcast();
-        console.log("Transfer GT No. %d from %s to %s", gtId, userAddr, toAddr);
+        // console.log("Transfer GT No. %d from %s to %s", gtId, userAddr, toAddr);
     }
+
     function printMarketConfig() public view {
         MarketConfig memory config = market.config();
         console.log("--- Market Config ---");
