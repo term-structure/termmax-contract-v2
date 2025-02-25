17f4c4ca80bb50fbbff83495c3be165c1691b50e
Aaron C.-W. Liang <aaron.liang@tkspring.com>
2025-02-25 15:50:37 +0800

    update arb sepolia deployment script

---
+++ script/deploy/ArbSepolia/DeployCoreArbSepolia.s.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {TermMaxFactory} from "contracts/factory/TermMaxFactory.sol";
+import {ITermMaxFactory} from "contracts/factory/ITermMaxFactory.sol";
+import {ITermMaxRouter} from "contracts/router/ITermMaxRouter.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {TermMaxMarket} from "contracts/TermMaxMarket.sol";
+import {MockERC20} from "contracts/test/MockERC20.sol";
+import {MockPriceFeed} from "contracts/test/MockPriceFeed.sol";
+import {MockPriceFeed} from "contracts/test/MockPriceFeed.sol";
+import {MarketConfig} from "contracts/storage/TermMaxStorage.sol";
+import {IMintableERC20} from "contracts/tokens/IMintableERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SwapAdapter} from "contracts/test/testnet/SwapAdapter.sol";
+import {Faucet} from "contracts/test/testnet/Faucet.sol";
+import {DeployBase} from "../DeployBase.s.sol";
+import {IOracle} from "contracts/oracle/IOracle.sol";
+import {VaultFactory, IVaultFactory} from "contracts/factory/VaultFactory.sol";
+
+contract DeployCoreArbSepolia is DeployBase {
+    // admin config
+    uint256 deployerPrivateKey = vm.envUint("ARB_SEPOLIA_DEPLOYER_PRIVATE_KEY");
+    address deployerAddr = vm.addr(deployerPrivateKey);
+    address adminAddr = vm.envAddress("ARB_SEPOLIA_ADMIN_ADDRESS");
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        (
+            ITermMaxFactory factory,
+            IVaultFactory vaultFactory,
+            IOracle oracleAggregator,
+            ITermMaxRouter router,
+            SwapAdapter swapAdapter,
+            Faucet faucet
+        ) = deployCore(adminAddr);
+        vm.stopBroadcast();
+
+        console.log("===== Git Info =====");
+        console.log("Git branch:", getGitBranch());
+        console.log("Git commit hash:");
+        console.logBytes(getGitCommitHash());
+        console.log();
+
+        console.log("===== Core Info =====");
+        console.log("Deplyer:", deployerAddr);
+        console.log("Admin:", adminAddr);
+        console.log("Factory deployed at:", address(factory));
+        console.log("VaultFactory deployed at:", address(vaultFactory));
+        console.log("Oracle Aggregator deployed at:", address(oracleAggregator));
+        console.log("Router deployed at:", address(router));
+        console.log("SwapAdapter deployed at:", address(swapAdapter));
+        console.log("Faucet deployed at:", address(faucet));
+    }
+}
---
+++ script/deploy/ArbSepolia/DeployMarketArbSepolia.s.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {TermMaxFactory} from "contracts/factory/TermMaxFactory.sol";
+import {ITermMaxFactory} from "contracts/factory/ITermMaxFactory.sol";
+import {TermMaxRouter} from "contracts/router/TermMaxRouter.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {TermMaxMarket} from "contracts/TermMaxMarket.sol";
+import {MockERC20} from "contracts/test/MockERC20.sol";
+import {MockPriceFeed} from "contracts/test/MockPriceFeed.sol";
+import {MockPriceFeed} from "contracts/test/MockPriceFeed.sol";
+import {MarketConfig} from "contracts/storage/TermMaxStorage.sol";
+import {IMintableERC20} from "contracts/tokens/IMintableERC20.sol";
+import {IGearingToken} from "contracts/tokens/IGearingToken.sol";
+import {IOracle} from "contracts/oracle/IOracle.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {MockSwapAdapter} from "contracts/test/MockSwapAdapter.sol";
+import {JsonLoader} from "../../utils/JsonLoader.sol";
+import {Faucet} from "contracts/test/testnet/Faucet.sol";
+import {FaucetERC20} from "contracts/test/testnet/FaucetERC20.sol";
+import {DeployBase} from "../DeployBase.s.sol";
+
+contract DeloyMarketArbSepolia is DeployBase {
+    // admin config
+    uint256 deployerPrivateKey = vm.envUint("ARB_SEPOLIA_DEPLOYER_PRIVATE_KEY");
+    address deployerAddr = vm.addr(deployerPrivateKey);
+    address adminAddr = vm.envAddress("ARB_SEPOLIA_ADMIN_ADDRESS");
+    address priceFeedOperatorAddr = vm.envAddress("ARB_SEPOLIA_PRICE_FEED_OPERATOR_ADDRESS");
+
+    // address config
+    address factoryAddr = address(0x5cB28BcB9dE6C10E85E1CBA8A4A59D82c5CD1630);
+    address oracleAddr = address(0x54505e5E2C23ad39ED06971A5d6410F501641F84);
+    address routerAddr = address(0xA3e778D80D04e304fefd613c2e1675eE056136cf);
+    address faucetAddr = address(0x05bC1c5c5Ede8722aAF89663F50a3148EaE69b21);
+
+    function run() public {
+        uint256 currentBlockNum = block.number;
+        Faucet faucet = Faucet(faucetAddr);
+        string memory deployDataPath = string.concat(vm.projectRoot(), "/script/deploy/deploydata/arbSepolia.json");
+        vm.startBroadcast(deployerPrivateKey);
+        (TermMaxMarket[] memory markets, JsonLoader.Config[] memory configs) = deployMarkets(
+            factoryAddr, oracleAddr, routerAddr, faucetAddr, deployDataPath, adminAddr, priceFeedOperatorAddr
+        );
+
+        console.log("Faucet token number:", faucet.tokenNum());
+
+        vm.stopBroadcast();
+
+        console.log("===== Git Info =====");
+        console.log("Git branch:", getGitBranch());
+        console.log("Git commit hash:");
+        console.logBytes(getGitCommitHash());
+        console.log();
+
+        console.log("===== Address Info =====");
+        console.log("Deplyer:", deployerAddr);
+        console.log("Price Feed Operator:", priceFeedOperatorAddr);
+        console.log("Deployed at block number:", currentBlockNum);
+        console.log("");
+
+        for (uint256 i = 0; i < markets.length; i++) {
+            console.log("===== Market Info - %d =====", i);
+            printMarketConfig(faucet, markets[i], configs[i].salt);
+            console.log("");
+        }
+    }
+
+    function printMarketConfig(Faucet faucet, TermMaxMarket market, uint256 salt) public view {
+        MarketConfig memory marketConfig = market.config();
+        (IMintableERC20 ft, IMintableERC20 xt, IGearingToken gt, address collateralAddr, IERC20 underlying) =
+            market.tokens();
+
+        Faucet.TokenConfig memory collateralConfig = faucet.getTokenConfig(faucet.getTokenId(collateralAddr));
+
+        Faucet.TokenConfig memory underlyingConfig = faucet.getTokenConfig(faucet.getTokenId(address(underlying)));
+
+        console.log("Market deployed at:", address(market));
+        console.log("Collateral (%s) deployed at: %s", IERC20Metadata(collateralAddr).symbol(), address(collateralAddr));
+        console.log(
+            "Underlying (%s) deployed at: %s", IERC20Metadata(address(underlying)).symbol(), address(underlying)
+        );
+        console.log("Collateral price feed deployed at:", address(collateralConfig.priceFeedAddr));
+        console.log("Underlying price feed deployed at:", address(underlyingConfig.priceFeedAddr));
+
+        console.log("FT deployed at:", address(ft));
+        console.log("XT deployed at:", address(xt));
+        console.log("GT deployed at:", address(gt));
+
+        console.log();
+
+        console.log("Treasurer:", marketConfig.treasurer);
+        console.log("Maturity:", marketConfig.maturity);
+        console.log("Salt:", salt);
+        console.log("Lend Taker Fee Ratio:", marketConfig.feeConfig.lendTakerFeeRatio);
+        console.log("Lend Maker Fee Ratio:", marketConfig.feeConfig.lendMakerFeeRatio);
+        console.log("Borrow Taker Fee Ratio:", marketConfig.feeConfig.borrowTakerFeeRatio);
+        console.log("Borrow Maker Fee Ratio:", marketConfig.feeConfig.borrowMakerFeeRatio);
+        console.log("Issue FT Fee Ratio:", marketConfig.feeConfig.issueFtFeeRatio);
+        console.log("Issue FT Fee Ref:", marketConfig.feeConfig.issueFtFeeRef);
+    }
+}
