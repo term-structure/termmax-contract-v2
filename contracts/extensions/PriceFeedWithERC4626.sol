24b1937ac313a55218d72fe9a13ecd573699b4a9
evan <evan@gmail.com>
2025-03-21 12:19:00 +0800

    ERC4626 price feed

--- contracts/extensions/PriceFeedWithERC4626.sol
+++ contracts/extensions/PriceFeedWithERC4626.sol
@@ -2,23 +2,25 @@
 pragma solidity ^0.8.27;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {MathLib} from "contracts/lib/MathLib.sol";
 
 contract PriceFeedConverter is AggregatorV3Interface {
     using MathLib for *;
-
     error GetRoundDataNotSupported();
 
-    AggregatorV3Interface public immutable aTokenToBTokenPriceFeed;
-    AggregatorV3Interface public immutable bTokenToCTokenPriceFeed;
-
+    AggregatorV3Interface public immutable assetPriceFeed;
+    IERC4626 public immutable vault;
     int256 immutable priceDemonitor;
+    uint256 immutable vaultDemonitor;
 
-    constructor(address _aTokenToBTokenPriceFeed, address _bTokenToCTokenPriceFeed) {
-        aTokenToBTokenPriceFeed = AggregatorV3Interface(_aTokenToBTokenPriceFeed);
-        bTokenToCTokenPriceFeed = AggregatorV3Interface(_bTokenToCTokenPriceFeed);
-        priceDemonitor =
-            int256(10 ** aTokenToBTokenPriceFeed.decimals()) * int256(10 ** bTokenToCTokenPriceFeed.decimals());
+    constructor(address _assetPriceFeed, address _vault) {
+        assetPriceFeed = AggregatorV3Interface(_assetPriceFeed);
+        vault = IERC4626(_vault);
+        vaultDemonitor = 10 ** vault.decimals();
+        uint256 assetDemonitor = 10 ** IERC20Metadata(vault.asset()).decimals();
+        priceDemonitor = int256(10 ** assetPriceFeed.decimals()) * int256(assetDemonitor);
     }
 
     function decimals() public view returns (uint8) {
@@ -26,13 +28,11 @@ contract PriceFeedConverter is AggregatorV3Interface {
     }
 
     function description() external view returns (string memory) {
-        return string(
-            abi.encodePacked(aTokenToBTokenPriceFeed.description(), " - ", bTokenToCTokenPriceFeed.description())
-        );
+        return string(abi.encodePacked("Price Feed for ", vault.name()));
     }
 
     function version() external view returns (uint256) {
-        return aTokenToBTokenPriceFeed.version().min(bTokenToCTokenPriceFeed.version());
+        return assetPriceFeed.version();
     }
 
     function getRoundData(uint80 /* _roundId */ )
@@ -52,10 +52,9 @@ contract PriceFeedConverter is AggregatorV3Interface {
 
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
         (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
-            aTokenToBTokenPriceFeed.latestRoundData();
-        (, int256 answer2, uint256 startedAt2, uint256 updatedAt2,) = bTokenToCTokenPriceFeed.latestRoundData();
-        // tokenPrice = answer * answer2
-        answer = answer * answer2 * int256((10 ** decimals())) / priceDemonitor;
-        return (roundId, answer, startedAt.min(startedAt2), updatedAt.min(updatedAt2), answeredInRound);
+            assetPriceFeed.latestRoundData();
+        uint256 vaultAnswer = vault.previewRedeem(vaultDemonitor).min(vault.convertToAssets(vaultDemonitor));
+        answer = answer * int256(vaultAnswer) * int256((10 ** decimals())) / priceDemonitor;
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
     }
 }
