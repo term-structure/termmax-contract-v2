dab373521192d54d171ccd8d4d5ff24f08be707a
abner.huang <fantasrulai@gmail.com>
2025-03-21 19:32:27 +0800

    update(MarketViewer): getVaultInfo

--- contracts/router/MarketViewer.sol
+++ contracts/router/MarketViewer.sol
@@ -8,13 +8,27 @@ import {ITermMaxOrder} from "contracts/ITermMaxOrder.sol";
 import {IMintableERC20} from "contracts/tokens/IMintableERC20.sol";
 import {IGearingToken} from "contracts/tokens/IGearingToken.sol";
 import {ITermMaxOrder} from "contracts/ITermMaxOrder.sol";
-import {OrderConfig, CurveCuts, FeeConfig} from "contracts/storage/TermMaxStorage.sol";
+import {OrderConfig, CurveCuts, FeeConfig, GtConfig} from "contracts/storage/TermMaxStorage.sol";
+import {ITermMaxVault} from "contracts/vault/ITermMaxVault.sol";
+import {OrderInfo} from "contracts/vault/VaultStorage.sol";
+import {PendingAddress, PendingUint192} from "contracts/lib/PendingLib.sol";
 
 contract MarketViewer {
     struct LoanPosition {
         uint256 loanId;
         uint256 collateralAmt;
         uint256 debtAmt;
+    }
+
+    struct LoanPositionV2 {
+        address owners;
+        uint256 loanId;
+        uint256 collateralAmt;
+        uint256 debtAmt;
+        uint128 ltv;
+        bool isHealthy;
+        bool isLiquidable;
+        uint128 maxRepayAmt;
     }
 
     struct Position {
@@ -34,6 +48,37 @@ contract MarketViewer {
         uint256 gtId;
         CurveCuts curveCuts;
         FeeConfig feeConfig;
+    }
+
+    struct VaultInfo {
+        string name;
+        string symbol;
+        address assetAddress;
+        // Basic vault metrics
+        uint256 totalAssets;
+        uint256 totalSupply;
+        uint256 apr;
+        // Governance settings
+        address guardian;
+        address curator;
+        uint256 timelock;
+        uint256 maxDeposit;
+        uint64 performanceFeeRate;
+        uint256 idleFunds; // asset.balanceOf(address(this))
+        // Financial metrics
+        uint256 totalFt;
+        uint256 accretingPrincipal;
+        uint256 annualizedInterest;
+        uint256 performanceFee;
+        // Queue information
+        address[] supplyQueue;
+        address[] withdrawQueue;
+        // Pending governance updates
+        PendingAddress pendingGuardian;
+        PendingUint192 pendingTimelock;
+        PendingUint192 pendingPerformanceFeeRate;
+        uint256 maxMint; // maxMint(address(0))
+        uint256 convertToSharesPrice; // convertToShares(One)
     }
 
     function getPositionDetail(ITermMaxMarket market, address owner) public view returns (Position memory position) {
@@ -71,15 +116,35 @@ contract MarketViewer {
 
     function getAllLoanPosition(ITermMaxMarket market, address owner) external view returns (LoanPosition[] memory) {
         (,, IGearingToken gt,,) = market.tokens();
-        IERC721Enumerable gtNft = IERC721Enumerable(address(gt));
-        uint256 balance = gtNft.balanceOf(owner);
+        uint256 balance = gt.balanceOf(owner);
         LoanPosition[] memory loanPositions = new LoanPosition[](balance);
         for (uint256 i = 0; i < balance; ++i) {
-            uint256 loanId = gtNft.tokenOfOwnerByIndex(owner, i);
+            uint256 loanId = gt.tokenOfOwnerByIndex(owner, i);
             (, uint128 debtAmt,, bytes memory collateralData) = gt.loanInfo(loanId);
             loanPositions[i].loanId = loanId;
             loanPositions[i].debtAmt = debtAmt;
             loanPositions[i].collateralAmt = _decodeAmount(collateralData);
+        }
+        return loanPositions;
+    }
+
+    function getAllLoanPositionV2(ITermMaxMarket market) external view returns (LoanPositionV2[] memory) {
+        (,, IGearingToken gtNft,,) = market.tokens();
+        GtConfig memory config = gtNft.getGtConfig();
+        uint256 supply = gtNft.totalSupply();
+        LoanPositionV2[] memory loanPositions = new LoanPositionV2[](supply);
+        for (uint256 i = 0; i < supply; ++i) {
+            uint256 loanId = gtNft.tokenByIndex(i);
+            (address owner, uint128 debtAmt, uint128 ltv, bytes memory collateralData) = gtNft.loanInfo(loanId);
+            (bool isLiquidable, uint128 maxRepayAmt) = gtNft.getLiquidationInfo(loanId);
+            loanPositions[i].loanId = loanId;
+            loanPositions[i].debtAmt = debtAmt;
+            loanPositions[i].collateralAmt = _decodeAmount(collateralData);
+            loanPositions[i].owners = gtNft.ownerOf(loanId);
+            loanPositions[i].ltv = ltv;
+            loanPositions[i].isHealthy = ltv >= config.loanConfig.liquidationLtv;
+            loanPositions[i].isLiquidable = isLiquidable;
+            loanPositions[i].maxRepayAmt = maxRepayAmt;
         }
         return loanPositions;
     }
@@ -103,6 +168,78 @@ contract MarketViewer {
         orderState.curveCuts = orderConfig.curveCuts;
         orderState.feeConfig = orderConfig.feeConfig;
         return orderState;
+    }
+
+    /**
+     * @notice Get comprehensive information about a TermMaxVault
+     * @param vault The TermMaxVault to query
+     * @return vaultInfo The vault information
+     */
+    function getVaultInfo(ITermMaxVault vault) external view returns (VaultInfo memory vaultInfo) {
+        IERC20 asset = IERC20(vault.asset());
+
+        // Basic vault metrics
+        vaultInfo.name = vault.name();
+        vaultInfo.symbol = vault.symbol();
+        vaultInfo.assetAddress = address(asset);
+        vaultInfo.totalAssets = vault.totalAssets();
+        vaultInfo.totalSupply = vault.totalSupply();
+        vaultInfo.apr = vault.apr();
+        
+        // Governance settings
+        vaultInfo.guardian = vault.guardian();
+        vaultInfo.curator = vault.curator();
+        vaultInfo.timelock = vault.timelock();
+        vaultInfo.maxDeposit = vault.maxDeposit(address(0));
+
+        vaultInfo.idleFunds = asset.balanceOf(address(vault));
+        
+        // Financial metrics
+        vaultInfo.totalFt = vault.totalFt();
+        vaultInfo.accretingPrincipal = vault.accretingPrincipal();
+        vaultInfo.annualizedInterest = vault.annualizedInterest();
+        vaultInfo.performanceFeeRate = vault.performanceFeeRate();
+        vaultInfo.performanceFee = vault.performanceFee();
+        
+        // Queue information
+        uint256 supplyQueueLength = vault.supplyQueueLength();
+        vaultInfo.supplyQueue = new address[](supplyQueueLength);
+        for (uint256 i = 0; i < supplyQueueLength; i++) {
+            vaultInfo.supplyQueue[i] = vault.supplyQueue(i);
+        }
+        
+        uint256 withdrawQueueLength = vault.withdrawQueueLength();
+        vaultInfo.withdrawQueue = new address[](withdrawQueueLength);
+        for (uint256 i = 0; i < withdrawQueueLength; i++) {
+            vaultInfo.withdrawQueue[i] = vault.withdrawQueue(i);
+        }
+        
+        // Pending governance updates
+        vaultInfo.pendingGuardian = vault.pendingGuardian();
+        vaultInfo.pendingTimelock = vault.pendingTimelock();
+        vaultInfo.pendingPerformanceFeeRate = vault.pendingPerformanceFeeRate();
+
+        vaultInfo.maxMint = vault.maxMint(address(0));
+        uint256 one = 10 ** vault.decimals();
+        vaultInfo.convertToSharesPrice = vault.convertToShares(one);
+    }
+    
+    /**
+     * @notice Get information about all orders in a vault
+     * @param vault The TermMaxVault to query
+     * @return orderInfos Array of information about each order in the vault
+     */
+    function getVaultOrdersInfo(ITermMaxVault vault) external view returns (OrderState[] memory) {
+        uint256 supplyQueueLength = vault.supplyQueueLength();
+        OrderState[] memory orderInfos = new OrderState[](supplyQueueLength);
+        
+        for (uint256 i = 0; i < supplyQueueLength; i++) {
+            address orderAddress = vault.supplyQueue(i);
+            ITermMaxOrder order = ITermMaxOrder(orderAddress);
+            orderInfos[i] = this.getOrderState(order);
+        }
+        
+        return orderInfos;
     }
 
     function _decodeAmount(bytes memory collateralData) internal pure returns (uint256) {
