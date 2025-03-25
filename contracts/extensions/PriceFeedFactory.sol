348d041c03cddc71f79dafd890a2f6c18e9c806f
evan <evan@gmail.com>
2025-03-25 15:25:13 +0800

    add event to pricefeed factory

--- contracts/extensions/PriceFeedFactory.sol
+++ contracts/extensions/PriceFeedFactory.sol
@@ -6,21 +6,29 @@ import {PriceFeedConverter} from "./PriceFeedConverter.sol";
 import {PTWithPriceFeed} from "./PTWithPriceFeed.sol";
 
 contract PriceFeedFactory {
+    event PriceFeedCreated(address indexed priceFeed);
+
     function createPriceFeedWithERC4626(address _assetPriceFeed, address _vault) external returns (address) {
-        return address(new PriceFeedWithERC4626(_assetPriceFeed, _vault));
+        address priceFeed = address(new PriceFeedWithERC4626(_assetPriceFeed, _vault));
+        emit PriceFeedCreated(priceFeed);
+        return priceFeed;
     }
 
     function createPriceFeedConverter(address _aTokenToBTokenPriceFeed, address _bTokenToCTokenPriceFeed)
         external
         returns (address)
     {
-        return address(new PriceFeedConverter(_aTokenToBTokenPriceFeed, _bTokenToCTokenPriceFeed));
+        address priceFeed = address(new PriceFeedConverter(_aTokenToBTokenPriceFeed, _bTokenToCTokenPriceFeed));
+        emit PriceFeedCreated(priceFeed);
+        return priceFeed;
     }
 
     function createPTWithPriceFeed(address _pendlePYLpOracle, address _market, uint32 _duration, address _priceFeed)
         external
         returns (address)
     {
-        return address(new PTWithPriceFeed(_pendlePYLpOracle, _market, _duration, _priceFeed));
+        address priceFeed = address(new PTWithPriceFeed(_pendlePYLpOracle, _market, _duration, _priceFeed));
+        emit PriceFeedCreated(priceFeed);
+        return priceFeed;
     }
 }
