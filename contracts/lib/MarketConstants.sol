a9e3394e57d702475b885300174e43ff0a78aa64
Evan <evan.wang@tkspring.com>
2025-06-03 12:19:55 +0800

    remove old contracts

--- contracts/lib/MarketConstants.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-library MarketConstants {
-    string constant PREFIX_FT = "FT:";
-    string constant PREFIX_XT = "XT:";
-    string constant PREFIX_GT = "GT:";
-    string constant PREFIX_MARKET = "Termmax Market:";
-}
--- contracts/lib/StringUtil.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-library StringUtil {
-    function contact(string memory a, string memory b) internal pure returns (string memory) {
-        return string(abi.encodePacked(a, b));
-    }
-}
