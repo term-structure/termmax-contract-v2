e4fcb2fcb6484ec25e83011bab425ce8edb3b2ea
Evan <evan.wang@tkspring.com>
2025-06-20 12:49:59 +0800

    sepecial adapters for v1 router

---
+++ contracts/v1/router/temAdapters/OdosV2AdapterV2.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC20SwapAdapterV2.sol";
+
+/**
+ * @title OdosRouterV2 interface
+ */
+interface IOdosRouterV2 {
+    /// @notice Struct to hold swap token information
+    struct SwapTokenInfo {
+        address inputToken;
+        uint256 inputAmount;
+        address inputReceiver;
+        address outputToken;
+        uint256 outputQuote;
+        uint256 outputMin;
+        address outputReceiver;
+    }
+
+    function swap(SwapTokenInfo memory tokenInfo, bytes calldata pathDefinition, address executor, uint32 referralCode)
+        external
+        payable
+        returns (uint256 amountOut);
+}
+
+/**
+ * @title TermMax OdosAdapterV2AdapterV2
+ * @author Term Structure Labs
+ */
+contract OdosV2AdapterV2 is ERC20SwapAdapterV2 {
+    using TransferUtilsV2 for IERC20;
+    using Math for uint256;
+
+    IOdosRouterV2 public immutable router;
+
+    constructor(address router_) {
+        router = IOdosRouterV2(router_);
+    }
+
+    function _swap(IERC20 tokenIn, IERC20, uint256 amountIn, bytes memory swapData)
+        internal
+        virtual
+        override
+        returns (uint256 tokenOutAmt)
+    {
+        tokenIn.safeIncreaseAllowance(address(router), amountIn);
+
+        (
+            IOdosRouterV2.SwapTokenInfo memory tokenInfo,
+            bytes memory pathDefinition,
+            address executor,
+            uint32 referralCode
+        ) = abi.decode(swapData, (IOdosRouterV2.SwapTokenInfo, bytes, address, uint32));
+
+        /**
+         * Note: Scaling Input/Output amount
+         */
+        tokenInfo.outputQuote = tokenInfo.outputQuote.mulDiv(amountIn, tokenInfo.inputAmount, Math.Rounding.Ceil);
+        tokenInfo.outputMin = tokenInfo.outputMin.mulDiv(amountIn, tokenInfo.inputAmount, Math.Rounding.Ceil);
+        tokenInfo.inputAmount = amountIn;
+        tokenInfo.outputReceiver = address(this);
+
+        tokenOutAmt = router.swap(tokenInfo, pathDefinition, executor, referralCode);
+    }
+}
---
+++ contracts/v1/router/temAdapters/PendleSwapV3AdapterV2.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IPAllActionV3} from "@pendle/core-v2/contracts/interfaces/IPAllActionV3.sol";
+import {IPMarket, IPPrincipalToken, IPYieldToken} from "@pendle/core-v2/contracts/interfaces/IPMarket.sol";
+import {PendleHelper} from "../../../v2/extensions/pendle/PendleHelper.sol";
+import "./ERC20SwapAdapterV2.sol";
+
+/**
+ * @title TermMax PendleSwapV3AdapterV2
+ * @author Term Structure Labs
+ */
+contract PendleSwapV3AdapterV2 is ERC20SwapAdapterV2, PendleHelper {
+    using TransferUtilsV2 for IERC20;
+    using Math for uint256;
+
+    IPAllActionV3 public immutable router;
+
+    constructor(address router_) {
+        router = IPAllActionV3(router_);
+    }
+
+    function _swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amount, bytes memory swapData)
+        internal
+        virtual
+        override
+        onlyProxy
+        returns (uint256 tokenOutAmt)
+    {
+        (address ptMarketAddr, uint256 inAmount, uint256 minTokenOut) =
+            abi.decode(swapData, (address, uint256, uint256));
+        IPMarket market = IPMarket(ptMarketAddr);
+
+        (, IPPrincipalToken PT,) = market.readTokens();
+        IERC20(tokenIn).safeIncreaseAllowance(address(router), amount);
+
+        /**
+         * Note: Scaling Input/Output amount
+         */
+        minTokenOut = minTokenOut.mulDiv(amount, inAmount, Math.Rounding.Ceil);
+        if (tokenOut == PT) {
+            (tokenOutAmt,,) = router.swapExactTokenForPt(
+                address(this),
+                address(market),
+                minTokenOut,
+                defaultApprox,
+                createTokenInputStruct(address(tokenIn), amount),
+                emptyLimit
+            );
+        } else {
+            if (PT.isExpired()) {
+                (tokenOutAmt,) = router.redeemPyToToken(
+                    address(this), PT.YT(), amount, createTokenOutputStruct(address(tokenOut), minTokenOut)
+                );
+            } else {
+                (tokenOutAmt,,) = router.swapExactPtForToken(
+                    address(this),
+                    address(market),
+                    amount,
+                    createTokenOutputStruct(address(tokenOut), minTokenOut),
+                    emptyLimit
+                );
+            }
+        }
+    }
+}
