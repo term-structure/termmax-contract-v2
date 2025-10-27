193ce4abddde076b46b9d568cb8fa252105f462f
Evan <evan.wang@tkspring.com>
2025-10-27 16:15:44 +0800

    feat: implement IKodiakRouter interface and KodiakSwapAdapter with swap functionality

---
+++ contracts/v2/router/swapAdapters/KodiakSwapAdapter.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IKodiakRouter} from "../../extensions/kodiak/IKodiakRouter.sol";
+import "./ERC20SwapAdapterV2.sol";
+
+/**
+ * @title TermMax KodiakSwapAdapter
+ * @author Term Structure Labs
+ */
+contract KodiakSwapAdapter is ERC20SwapAdapterV2 {
+    using TransferUtilsV2 for IERC20;
+    using Math for uint256;
+
+    function _swap(address recipient, IERC20 tokenIn, IERC20 tokenOut, uint256 amount, bytes memory swapData)
+        internal
+        virtual
+        override
+        returns (uint256 tokenOutAmt)
+    {
+        (
+            IKodiakRouter router,
+            IKodiakRouter.InputAmount memory input,
+            IKodiakRouter.OutputAmount memory output,
+            IKodiakRouter.SwapData memory swapDataStruct,
+            IKodiakRouter.FeeData memory feeData
+        ) = abi.decode(
+            swapData,
+            (
+                IKodiakRouter,
+                IKodiakRouter.InputAmount,
+                IKodiakRouter.OutputAmount,
+                IKodiakRouter.SwapData,
+                IKodiakRouter.FeeData
+            )
+        );
+        /**
+         * Note: Scaling Input/Output amount (round up)
+         */
+        output.minAmountOut = output.minAmountOut.mulDiv(amount, input.amount, Math.Rounding.Ceil);
+        input.amount = amount;
+        output.receiver = recipient;
+        uint256 outputAmountBefore = tokenOut.balanceOf(recipient);
+        tokenIn.safeApprove(address(router), amount);
+        router.swap(input, output, swapDataStruct, feeData);
+        tokenOutAmt = tokenOut.balanceOf(recipient) - outputAmountBefore;
+    }
+}
