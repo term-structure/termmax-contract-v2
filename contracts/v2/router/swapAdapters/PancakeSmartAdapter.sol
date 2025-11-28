2e2fef308202cb72b335a9984db5b9a924d87d48
Evan <evan.wang@tkspring.com>
2025-11-28 11:27:04 +0800

    optimize codes

--- contracts/v2/router/swapAdapters/PancakeSmartAdapter.sol
+++ contracts/v2/router/swapAdapters/PancakeSmartAdapter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "./ERC20SwapAdapterV2.sol";
 
 /**
